77e3802719270a80e5484c99b2d2e8b550c3e49f
Sergey <posergey1995@gmail.com>
2022-01-14 20:15:02 +0200

    ...

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts#, :authors
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'posts#index'
 
   get 'authors', to: 'posts#authors'
-  get 'author', to: 'posts#show_author'
+  get 'author/:id', to: 'posts#show_author', as: 'author'
 
 end
