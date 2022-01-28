ad5d1e25097589939bba61c9f7517fdf54082381
Sergey <posergey1995@gmail.com>
2022-01-28 23:54:25 +0200

    ...

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :sessions
   resources :posts do
     resources :comments do
-      get 'publish', to: 'comments#publish', as: 'publish'
+      post 'publish', to: 'comments#publish', as: 'publish'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
