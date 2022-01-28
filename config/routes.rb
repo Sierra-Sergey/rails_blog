13b71fab5f15c5976d691c45f45e572923ba8a6a
Sergey <posergey1995@gmail.com>
2022-01-28 22:30:01 +0200

    fix route for publish action

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :sessions
   resources :posts do
     resources :comments do
-      post 'publish', to: 'comments#publish', as: 'publish'
+      get 'publish', to: 'comments#publish', as: 'publish'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
