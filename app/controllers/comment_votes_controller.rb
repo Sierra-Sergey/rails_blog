79ee50507e0aa570900b8b632c0e8bbf74034437
Sergey <posergey1995@gmail.com>
2022-02-01 17:32:57 +0200

    fix votes, add ajax for votes

--- app/controllers/comment_votes_controller.rb
+++ app/controllers/comment_votes_controller.rb
@@ -1,31 +1,43 @@
 class CommentVotesController < ApplicationController
-  before_action :set_comment_vote, only: %i[ update ]
-  before_action :authorize, only: %i[ create update ]
+  before_action :authorize, only: %i[ vote ]
+  before_action :set_comment_vote, only: %i[ vote ]
 
-  def create
-    @vote = current_author.comment_votes.build(vote_params)
-    @vote[:author_id] = current_author.id
-      if @vote.save
-        redirect_to @vote.comment.post, notice: 'VOTE accepted'
-      end
-  end
-
-  def update
-    if @vote.vote_value == vote_params[:vote_value]
-      @vote.destroy
+  def vote
+    if vote_exist?
+      update_vote
     else
-      @vote.update(vote_params)
+      create_vote
     end
-    redirect_to @vote.comment.post, notice: 'VOTE changed'
+    respond_to do |format|
+      format.js
+    end
+    # redirect_to @vote.comment.post
   end
 
   private
 
   def set_comment_vote
-    @vote = CommentVote.find(params[:id])
+    @vote = CommentVote.find_by(author_id: current_author.id, comment_id: vote_params[:comment_id])
   end
 
   def vote_params
     params.require(:comment_vote).permit(:comment_id, :vote_value)
+  end
+
+  def create_vote
+    @vote = current_author.comment_votes.build(vote_params)
+    @vote.save
+  end
+
+  def update_vote
+    if @vote[:vote_value] == vote_params[:vote_value]
+      @vote.destroy
+    else
+      @vote.update(vote_params)
+    end
+  end
+
+  def vote_exist?
+    CommentVote.where(author_id: current_author.id, comment_id: vote_params[:comment_id]).exists?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,13 @@ Rails.application.routes.draw do
       post 'publish', to: 'comments#publish', as: 'publish'
     end
   end
-  resources :comment_votes, only: %i[create update destroy]
+  # resources :comment_votes, only: %i[create update destroy]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'posts#index'
   # post 'posts/:id/comments/:id/publish', to: 'comments#publish', as: 'publish'
 
+  post 'comment_vote', to: 'comment_votes#vote'
   post 'posts/:id', to: 'posts#show'
   get 'search', to: 'posts#search'
   get 'signup', to: 'authors#new', as: 'signup'
